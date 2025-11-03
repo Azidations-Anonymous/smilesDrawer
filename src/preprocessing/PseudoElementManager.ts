3004c22ffbbb16ccb55ed5e9c6cafb9b58547fe1
CMT <qataleaf@gmail.com>
2025-11-03 11:38:56 +1100

    feat: implement PR #211 (pseudo-element detection) and PR #210 (backbone angles)

--- src/preprocessing/PseudoElementManager.ts
+++ src/preprocessing/PseudoElementManager.ts
@@ -119,9 +119,17 @@ class PseudoElementManager {
 
             const pseudoElements = neighbour.getAttachedPseudoElements();
 
-            if (pseudoElements.hasOwnProperty('0O') && pseudoElements.hasOwnProperty('3C')) {
-              neighbour.isDrawn = false;
-              vertex.value.attachPseudoElement('Ac', '', 0);
+            if (element === 'C' && pseudoElements.hasOwnProperty('0O') && pseudoElements.hasOwnProperty('3C')) {
+              if (pseudoElements['0O'].count === 1 && pseudoElements['3C'].count === 1) {
+                neighbour.isDrawn = false;
+                vertex.value.attachPseudoElement('Ac', '', 0);
+              }
+            }
+            else if (element === 'S' && pseudoElements.hasOwnProperty('0O') && pseudoElements.hasOwnProperty('3C')) {
+              if (pseudoElements['0O'].count === 2 && pseudoElements['3C'].count === 1) {
+                neighbour.isDrawn = false;
+                vertex.value.attachPseudoElement('Ms', '', 0);
+              }
             }
           }
         }
